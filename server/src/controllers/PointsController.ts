e285bc6e69262a3ecbf5667ef0ad844f802a3290
luizfverissimo <luizfverissimo@gmail.com>
2020-06-07 22:04:03 -0300

    finalização da aula 2

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -2,6 +2,25 @@ import {Request, Response} from 'express'
 import knex from "../database/connections"
 
 class PointsController {
+  async index(request: Request, response: Response){
+    //filtros: cidade, uf, items - (query params)
+
+    const {city, uf, items} = request.query
+    const paresedItems = String(items)
+      .split(',')
+      .map(item => Number(item.trim()))
+
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', paresedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*')
+
+    return response.json(points)
+  }
+
   async show(request: Request, response: Response){
     const { id } = request.params
 
@@ -34,7 +53,7 @@ class PointsController {
     const trx = await knex.transaction()
 
     const point = {
-      image: "image-placeholder",
+      image: "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
       name,
       email,
       whatsapp,
@@ -56,6 +75,7 @@ class PointsController {
     })
   
     await trx('point_items').insert(pointItems)
+    await trx.commit()
   
     return response.json({
       id: point_id,
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 
-import PointsController from "./controllers/pointsController"
-import ItemsController from "./controllers/itemsController"
+import PointsController from "./controllers/PointsController"
+import ItemsController from "./controllers/ItemsController"
 
 const routes = express.Router();
 const pointsController = new PointsController()
@@ -9,6 +9,7 @@ const itemsController = new ItemsController()
 
 routes.get("/items", itemsController.index);
 routes.get("/points/:id", pointsController.show);
+routes.get("/points", pointsController.index);
 
 routes.post('/points', pointsController.create)
 
