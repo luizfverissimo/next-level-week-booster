985ba2ef05b0af7a9a825354b7d860a3442b0679
luizfverissimo <luizfverissimo@gmail.com>
2020-06-08 19:09:08 -0300

    react arrumado

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div>
+        <Header title='Olá Mundão bom!' />
+        <h2>Conteúdo do site</h2>      
+    </div>
+  );
+}
+
+export default App;
